a78f3457bcabe23cdea83c087bfcfccc09b4948f
Catalina Barbu <barbu.catalina@yahoo.com>
2025-07-19 18:44:28 +0300

    Add method to remove products by name in ProductManager

--- product_manager.py
+++ product_manager.py
@@ -1,5 +1,3 @@
-from product import Product
-
 class ProductManager:
     def __init__(self):
         self.products = []
@@ -7,9 +5,5 @@ class ProductManager:
     def add_product(self, product):
         self.products.append(product)
 
-    def display_products(self):
-        for product in self.products:
-            product.display_info()
-
-    def total_value(self):
-        return sum(product.price * product.quantity for product in self.products)
+    def remove_product_by_name(self, product_name):
+        self.products = [p for p in self.products if p.name != product_name]
